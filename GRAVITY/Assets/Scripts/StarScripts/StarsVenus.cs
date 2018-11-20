4fe690cd70424e0868f79cf7c64ff714eec090e8
PaulConstable <PaulConstable@users.noreply.github.com>
2018-11-20 15:18:18 +0000

    Menu

---
+++ GRAVITY/Assets/Scripts/StarScripts/StarsVenus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsVenus : MonoBehaviour {
+
+    GameManager gm; // Reference to the GameManager script
+
+    // Use this for initialization
+    void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>(); // "grab" the GameManager
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void OnTriggerEnter2D(Collider2D other) // when collsion with the stars do the following
+    {
+        if (other.tag == "Player")
+        {
+            gm.StarsVenus(); // call the function stars within the GameManager
+            Destroy(gameObject); // Destroy
+        }
+    }
+}
