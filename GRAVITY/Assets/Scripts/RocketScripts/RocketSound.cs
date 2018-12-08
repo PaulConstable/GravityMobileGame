60336a745b9049c56462b09aab7f1604acabda4a
K1614994 <K1614994@users.noreply.github.com>
2018-12-08 22:21:15 +0000

    RocketSoundScripts

---
+++ GRAVITY/Assets/Scripts/RocketScripts/RocketSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketSound : MonoBehaviour {
+
+    public AudioSource MusicSource;
+
+	// Use this for initialization
+	void Start () {
+        MusicSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            MusicSource.Play();
+        }
+    }
+}
