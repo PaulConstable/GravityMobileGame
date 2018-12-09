01451392d78aab8e8ad96c2663371605686f8f0a
K1614994 <K1614994@users.noreply.github.com>
2018-12-09 15:19:38 +0000

    More SoundWork

---
+++ GRAVITY/Assets/Scripts/EnemyScripts/DeathNoise.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathNoise : MonoBehaviour {
+
+    public AudioSource MusicSource;
+
+	// Use this for initialization
+	void Start () {
+        MusicSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            MusicSource.Play();
+        }
+    }
+
+}
