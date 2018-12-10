54f4d6429fc3d627b3ffd6b29ea0582a1efd73ac
PaulConstable <PaulConstable@users.noreply.github.com>
2018-12-10 23:55:36 +0000

    UFO beam

---
+++ GRAVITY/Assets/Blinker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinker : MonoBehaviour
+{
+
+    public GameObject flashing_Label;
+
+    public float interval;
+
+    void Start()
+    {
+        InvokeRepeating("FlashLabel", 0, interval);
+    }
+
+    void FlashLabel()
+    {
+        if (flashing_Label.activeSelf)
+            flashing_Label.SetActive(false);
+        else
+            flashing_Label.SetActive(true);
+    }
+}
