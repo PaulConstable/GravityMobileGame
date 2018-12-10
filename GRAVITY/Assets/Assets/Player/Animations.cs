c2a8b3bf4fa8249c6a063a80772e8981f533f98a
PaulConstable <PaulConstable@users.noreply.github.com>
2018-12-10 16:46:20 +0000

    Animations

---
+++ GRAVITY/Assets/Assets/Player/Animations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour {
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Hitbox")
+        {
+            anim.SetTrigger("FloatTrigger");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Hitbox")
+        {
+            anim.SetTrigger("WalkTrigger");
+        }
+    }
+}
