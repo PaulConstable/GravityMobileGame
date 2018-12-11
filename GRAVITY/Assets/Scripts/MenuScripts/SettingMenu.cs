aa8967cf2c2bb92192341bf3526b7d6649014476
PaulConstable <PaulConstable@users.noreply.github.com>
2018-12-11 17:38:03 +0000

    Sounds, Options

---
+++ GRAVITY/Assets/Scripts/MenuScripts/SettingMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class SettingMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer, audioMixer2;
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetVolume2(float volume)
+    {
+        audioMixer2.SetFloat("volume", volume);
+    }
+
+}
