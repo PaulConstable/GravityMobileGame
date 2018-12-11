fba60669134db70b924cc91f0418ffa8f09d0de9
K1614994 <K1614994@users.noreply.github.com>
2018-12-11 19:14:09 +0000

    AlmostDone

---
+++ GRAVITY/Assets/Assets/Sounds/GameSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameSound : MonoBehaviour
+{
+    private static GameSound instance = null;
+    public static GameSound Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
+
+
